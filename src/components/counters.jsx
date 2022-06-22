89a7276cbaa758550d8d5b85dc22af052cf01bc8
HarshitaS01 <87194955+codeharshita@users.noreply.github.com>
2022-06-22 12:04:21 +0530

    Update counters.jsx

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -59,3 +59,23 @@ class Counters extends Component {
 }
  
 export default Counters;
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+  const {counters, onReset, onDelete,onIncrement} = this.props
+        return (
+        <div>
+            <button 
+            onClick={onReset}
+            className="btn btn-primary btn-sm m-2">
+                Reset 
+            </button>
+            {counters.map(counter => (
+            <Counter key={counter.id} 
+            onDelete={onDelete} 
+            onIncrement ={onIncrement}
+            counter={counter} >
+                
+            </Counter>
+        ))}
+        </div>
