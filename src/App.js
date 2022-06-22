6aa52fa46231400749556d361fbad7db8c92ad38
HarshitaS01 <87194955+codeharshita@users.noreply.github.com>
2022-06-22 06:50:15 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,40 @@ import './App.css';
 import Counters from './components/counters'; 
 
 function App() {
+  
+  state = {  
+
+        counters:[
+            {id: 1 , value:4},
+            {id: 2 , value:0},
+            {id: 3 , value:0},
+            {id: 4 , value:0}
+         ]
+
+    };
+
+    handleIncrement = counter => {
+        const counters = [...this.state.counters]
+        const index = counters.indexOf(counter)
+        counters[index]={...counter};
+        counters[index].value++;
+        this.setState({counters});
+    };
+    
+  handleDelete = counterId => {
+    const counters = this.state.counters.filter(c => c.id !== counterId);
+    this.setState({counters});
+  };
+
+    handleReset=()=>{
+        const counters = this.state.counters.map(c =>{
+            c.value=0;
+            return c;
+        });
+        this.setState({counters});
+
+    };
+  
   return (
    <React.Fragment>
     
