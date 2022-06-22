2a4192ea6471662c3c16ae1d6335e62b92f2d6ee
HarshitaS01 <87194955+codeharshita@users.noreply.github.com>
2022-06-22 06:44:57 +0530

    Update counters.jsx

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -14,7 +14,13 @@ class Counters extends Component {
 
     };
 
-    
+    handleIncrement = counter => {
+        const counters = [...this.state.counters]
+        const index = counters.indexOf(counter)
+        counters[index]={...counter};
+        counters[index].value++;
+        this.setState({counters});
+    };
     
   handleDelete = counterId => {
     const counters = this.state.counters.filter(c => c.id !== counterId);
@@ -42,6 +48,7 @@ class Counters extends Component {
             {this.state.counters.map(counter => (
             <Counter key={counter.id} 
             onDelete={this.handleDelete} 
+            onIncrement ={this.handleIncrement}
             counter={counter} >
                 
             </Counter>
