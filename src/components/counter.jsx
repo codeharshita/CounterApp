574de5ff5f3ac349d29a8a98bf2b246161d63863
HarshitaS01 <87194955+codeharshita@users.noreply.github.com>
2022-06-22 21:19:50 +0530

    lifecycle hooks using cc

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,41 +1,41 @@
 import React, { Component } from 'react';
 
-
- class Counter extends Component {
-    state={
-        count:0,
-        tags: ["tag1","tag2","tag3"]
-    }
-
-    styles ={
-        fontSize: "25px",
-        fontWeight:"bold"
-       
-    }
-
-
-
- render() { 
-    return (
+class Counter extends Component {
+    
+    render() { 
+        console.log("Counter-Rendered");
+        return (
         <div> 
-            <span  className={this.getBadgeClasses()}>{this.incrCnt()}</span>
-            <button className='btn btn-seccondry btn-lg'>Increment</button>
-            <ul>{this.state.tags.map(tag => <li key ={tag}>{tag}</li>)}</ul>
-         </div>
+          
+             <span  className={this.getBadgeClasses()}>{this.incrCnt()}</span>
+            <button 
+            onClick= { () => this.props.onIncrement(this.props.counter) } 
+            className='btn btn-seccondry btn-sm'
+            >
+             Increment
+            </button>
+            <button 
+            onClick={ () => this.props.onDelete(this.props.counter.id)} 
+            className="btn btn-danger btn-sm m-2"
+            >
+                Delete
+            </button>
+        </div>
         );
     }
 
     getBadgeClasses() {
-         let classes = "badge m-2 badge-";
-         classes += this.state.count === 0 ? "warning" : "primary";
-         return classes;
-     }
-
-    incrCnt(){
-        const count = this.state.count
-        return count == 0 ? 'Zero': count;
-
+        let classes = "badge m-2 badge-";
+        classes += this.props.counter.value === 0 ? "warning" : "primary";
+        return classes;
     }
- }
-  
+
+   incrCnt(){
+       const {value} = this.props.counter;
+       return value === 0 ? "Zero": value;
+       
+
+   }
+}
+ 
  export default Counter;
