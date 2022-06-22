d44b35424784ad8e71d7701698a92c4cb7117a7f
HarshitaS01 <87194955+codeharshita@users.noreply.github.com>
2022-06-22 06:37:50 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import Counters from './components/counters'; 
 
 function App() {
   return (
    
     <NavBar/>
     <main className='container'>
-
+    <Counters/>
     </main>
    
   );
